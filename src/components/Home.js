f01b2753a2ebfe908dfa402445ef21bc0d79e4a3
tovebr <brandt.tove@gmail.com>
2022-02-04 13:39:25 +0100

    responsive css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import portrait from "../images/portrait.png";
-import { ReactComponent as Logo } from "../components/logo.svg";
+/* import { ReactComponent as Logo } from "../components/logo.svg"; */
+import { ReactComponent as LogoWithName } from "../components/logoName.svg";
 import "./Home.css";
 import NavAbout from "./NavAbout";
 import ContactLinks from "./ContactLinks";
@@ -10,13 +11,7 @@ const Home = () => {
     <div id='home' className='container'>
       <div className='home-content'>
         <div className='first'>
-          <div className='home-header'>
-            <Logo className='home-logo' />
-            <div className='home-name'>
-              <h2 className='first-name'>Tove </h2>
-              <h2 className='last-name'>Brandt</h2>
-            </div>
-          </div>
+          <LogoWithName />
         </div>
         <div className='second'>
           <img className='portrait' src={portrait} alt='portrait'></img>
