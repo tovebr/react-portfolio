affda1d182b475a555ab62905ebe8eb51ba6efb0
tovebr <brandt.tove@gmail.com>
2023-02-08 17:00:21 +0100

    removed unused var

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import About from './components/About';
-import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Code from './components/Code';
 import Music from './components/Music';
